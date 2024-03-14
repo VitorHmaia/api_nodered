9fd25b512d4bf870f2b8501bad214b8190293ad4
VitorHmaia <vitorhmaia@outlook.com>
2024-03-13 21:50:47 -0300

    chore: configure api to integrate with index html

--- api/api/urls.py
+++ api/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from myapp.views import DadosListCreate
+from mvt.views import DadosListCreate
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('api/dados/', DadosListCreate.as_view(), name='dados-list-create'),
+    path('', TemplateView.as_view(template_name='index.html'), name='index'),
 ]
