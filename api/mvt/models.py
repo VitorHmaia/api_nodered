cdfeaf14b5199fb7149ef313469fa27904d59a7d
VitorHmaia <vitorhmaia@outlook.com>
2024-03-13 21:29:50 -0300

    chore: create model and serializer

--- api/mvt/models.py
+++ api/mvt/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Dados(models.Model):
+    sensor = models.BooleanField(default=False)
+    botao = models.BooleanField(default=False)
+    liga_robo = models.BooleanField(default=False)
+    reset_contador = models.BooleanField(default=False)
+    valor_contagem = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"ID: {self.id}, Sensor: {self.sensor}, Botao: {self.botao}, LigaRobo: {self.liga_robo}, ResetContador: {self.reset_contador}, ValorContagem: {self.valor_contagem}"
--- api/mvt/views.py
+++ api/mvt/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import generics
+from .models import Dados
+from .serializers import DadosSerializer
+
+class DadosListCreate(generics.ListCreateAPIView):
+    queryset = Dados.objects.all()
+    serializer_class = DadosSerializer
