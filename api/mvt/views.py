f4cb94268b69d3f3bc779579fde5ae156e8abd5d
VitorHmaia <vitorhmaia@outlook.com>
2024-03-13 22:29:44 -0300

    chore: create function to catch datetime now and save it

--- api/mvt/views.py
+++ api/mvt/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
-
 from rest_framework import generics
 from .models import Dados
 from .serializers import DadosSerializer
+from datetime import datetime
 
 class DadosListCreate(generics.ListCreateAPIView):
     queryset = Dados.objects.all()
     serializer_class = DadosSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(created_at=datetime.now())
